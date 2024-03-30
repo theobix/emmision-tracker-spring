e5bae24de798f349df8179d33bcf559da304d7bc
FireGreeks <theodorbischler1@gmail.com>
2024-03-30 19:25:17 +0100

    Update GraphQlConfig.java

--- src/main/java/com/example/Emmision/Tracker/GraphQlConfig.java
+++ src/main/java/com/example/Emmision/Tracker/GraphQlConfig.java
@@ -20,7 +20,7 @@ public class GraphQlConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:9000");
+                registry.addMapping("/**").allowedOrigins("https://firegreeks.github.io");
             }
         };
     }

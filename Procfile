27c7d7011201953aa9a1f099bb7d1a9af48fbc79
FireGreeks <theodorbischler1@gmail.com>
2024-04-02 18:22:37 +0200

    Moved EmissionTrackerApplication

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java -Dserver.port=$PORT -jar build/libs/Emmision-Tracker-0.0.1-SNAPSHOT.jar
+web: java -Dserver.port=$PORT -jar build/libs/emission-tracker-0.0.1-SNAPSHOT.jar
--- src/main/java/com/example/emmisiontracker/EmmisionTrackerApplication.java
+++ src/main/java/com/example/emmisiontracker/EmmisionTrackerApplication.java
@@ -1,4 +1,4 @@
-package com.example.emmisiontracker.config;
+package com.example.emmisiontracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

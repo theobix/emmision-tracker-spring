f84c54bc9f596cf856665cb6a6d9da5aa3721a0d
FireGreeks <theodorbischler1@gmail.com>
2024-04-03 02:32:28 +0200

    Update EmissionTrackerApplication.java

--- src/main/java/com/example/emmisiontracker/EmissionTrackerApplication.java
+++ src/main/java/com/example/emmisiontracker/EmissionTrackerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EmmisionTrackerApplication {
+public class EmissionTrackerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmmisionTrackerApplication.class, args);
+		SpringApplication.run(EmissionTrackerApplication.class, args);
 	}
 
 }

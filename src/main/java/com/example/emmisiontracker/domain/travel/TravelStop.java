ed5dfd806e6aecd2d50a1cca21d29384f73e4f62
FireGreeks <theodorbischler1@gmail.com>
2024-04-23 11:29:52 +0200

    Made travelstop non nullable

--- src/main/java/com/example/emmisiontracker/domain/travel/TravelStop.java
+++ src/main/java/com/example/emmisiontracker/domain/travel/TravelStop.java
@@ -24,6 +24,7 @@ public class TravelStop {
     @JoinColumn(nullable=false)
     private Travel travel;
 
+    @Column(nullable = false)
     private TravelMethod travelMethod;
 
     @OneToOne(cascade = CascadeType.ALL)
